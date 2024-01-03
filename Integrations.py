a7c458f31b2e3df576e2bbd067455f4df3c1edbf
remi-epsi <152403194+remi-epsi@users.noreply.github.com>
2024-01-03 23:17:12 +0100

    Add files via upload

--- Integrations.py
+++ Integrations.py
@@ -2,8 +2,14 @@ import sys
 import pyodbc
 import requests
 
-
-end=0
+def verif_existe_sql(sql_query):
+    cursor.execute(sql_query)
+    requete = str(cursor.fetchone())
+    if requete == "('1',)":
+        res = 1
+    else:
+        res = 0  
+    return res
 
 # Informations de connexion à la BDD
 nom_serveur = sys.argv[1]
@@ -13,6 +19,7 @@ mot_de_passe = sys.argv[4]
 
 connexion_string = f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={nom_serveur};DATABASE={nom_base_de_donnees};UID={nom_utilisateur};PWD={mot_de_passe}'
 
+end=0
 
 while end != 1:
 
@@ -25,54 +32,75 @@ while end != 1:
         choix= input("\n Quelle inétragtion souhaitez-vous réaliser ? \n 1 - Intégrer un trigger qui se déclenche à l'intégration des codes INSEE (Taper 1) \n 2 - Intégrer une vue qui affiche les villes du Nord Pas de Calais (Taper 2) \n 3 - Ajouter les informations de population à la table (Taper 3) \n 4 - Souhaitez-vous quitter le programme ? (Taper 4) \n Votre choix : \n ")
 
         match choix:
-            #Créatio de la Vue
+            #Création de la table de log
             case "1":
-                with open('create_table.sql','r') as file:
-                    sql_query= file.read()
-                cursor.execute(sql_query)
-                print("Création de la table de log OK")
+                sql_exist = f"IF OBJECT_ID('log_codeINSEE', 'U') IS NOT NULL BEGIN SELECT '1'; END ELSE BEGIN SELECT '0'; END"
+                
+                if verif_existe_sql(sql_exist) == 0 :
+                    with open('create_table.sql','r') as file:
+                        sql_query= file.read()
+                    cursor.execute(sql_query)
+                    print("Création de la table de log OK")     
+                else:
+                    print("La table log_codeINSEE est déjà créé !")
 
-                with open('trigger.sql','r') as file:
-                    sql_query= file.read()
-                cursor.execute(sql_query)
-                print("Création du trigger OK \n")
+                sql_exist2 = f"IF OBJECT_ID('Trigger_CodeINSEE', 'TR') IS NOT NULL BEGIN SELECT '1'; END ELSE BEGIN SELECT '0'; END"
+                if verif_existe_sql(sql_exist2) == 0:
+                    #Crtéation du trigger
+                    with open('trigger.sql','r') as file:
+                        sql_query= file.read()
+                    cursor.execute(sql_query)
+                    print("Création du trigger OK \n")
+                else:
+                    print("Le trigger Trigger_CodeINSEE existe déjà")
+                
 
-            #Création du Trigger
+            #Création de la vue
             case "2":
-                with open('vue.sql', 'r') as file:
-                    sql_query= file.read()
-                cursor.execute(sql_query)
-                print("Création de la vue OK \n")
+                sql_exist3 = f"IF OBJECT_ID('vue_departement_59_62', 'V') IS NOT NULL BEGIN  SELECT '1'; END ELSE BEGIN SELECT '0'; END"
+                print(verif_existe_sql(sql_exist3))
+                if verif_existe_sql(sql_exist3) == 0:
+                    with open('vue.sql', 'r') as file:
+                        sql_query= file.read()
+                    cursor.execute(sql_query)
+                    print("Création de la vue OK \n")
+                else:
+                    print("La vue vue_departement_59_62 existe déjà !")
 
-            #Ajout de la colonne population
+            #Ajout de la colonne population et des données dans cette colonne
             case "3":
 
-                sql_query=f"ALTER TABLE Code_INSEE ADD popula NVARCHAR(50)"
-                cursor.execute(sql_query)
-                connexion.commit()
-                print("Ajout de la colonne population dans la table")
+                sql_exist = f"IF COL_LENGTH('Code_INSEE', 'popula') IS NOT NULL BEGIN  SELECT '1'; END ELSE BEGIN SELECT '0'; END"
 
-                api_url = "https://geo.api.gouv.fr/communes"
-                response = requests.get(api_url)
-                data = response.json()
+                if verif_existe_sql(sql_exist) == 0:
+                    sql_query=f"ALTER TABLE Code_INSEE ADD popula NVARCHAR(50)"
+                    cursor.execute(sql_query)
+                    connexion.commit()
+                    print("Ajout de la colonne population dans la table")
 
-                for commune in data:
-                    code_insee = commune.get("code")
-                    population = commune.get("population", None)
+                    api_url = "https://geo.api.gouv.fr/communes"
+                    response = requests.get(api_url)
+                    data = response.json()
 
-                    if code_insee[0] == '0':
-                        chaine_abrege = code_insee[1:]
-                    else:
-                        chaine_abrege = code_insee
+                    for commune in data:
+                        code_insee = commune.get("code")
+                        population = commune.get("population", None)
 
-                    if population != None:
-                        sql_query = f"UPDATE Code_INSEE SET popula = {population} WHERE code_commune_INSEE = '{chaine_abrege}';"
-                        
-                        print("Modification OK :" , chaine_abrege)
-                        cursor.execute(sql_query)
-                        connexion.commit()
-                
-                print("La colonne population est bien ajoutée \n")
+                        if code_insee[0] == '0':
+                            chaine_abrege = code_insee[1:]
+                        else:
+                            chaine_abrege = code_insee
+
+                        if population != None:
+                            sql_query = f"UPDATE Code_INSEE SET popula = {population} WHERE code_commune_INSEE = '{chaine_abrege}';"
+                            
+                            print("Modification OK :" , chaine_abrege)
+                            cursor.execute(sql_query)
+                            connexion.commit()
+                    
+                    print("La colonne population est bien ajoutée \n")
+                else:
+                    print("La colonne population existe déjà et les données dont intégrées !")
         
             case "4":
                 print("Bonne continuation ! \n")
@@ -80,7 +108,7 @@ while end != 1:
 
             #Choix autre que 1,2,3 ou 4
             case _:
-                print("Votre choix ne correspond pas à 1, 2 ou 3. Veuillez sélectionner l'un de ces trois choix. Le programme se stop.")
+                print("Votre choix ne correspond pas à 1, 2, 3 ou 4. Veuillez sélectionner l'un de ces trois choix. Le programme se stop.")
                 sys.exit()
 
         connexion.commit()
